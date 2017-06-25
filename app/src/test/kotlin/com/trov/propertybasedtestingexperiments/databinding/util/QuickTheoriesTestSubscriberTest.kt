d772f2d41747262b70b898ee8d2e3c5c09e9517a
jamesbluecrow <jamesbluecrow@gmail.com>
2017-06-25 21:07:23 +0100

    Cleaned property based tests using QuickTheories

--- app/src/test/kotlin/com/trov/propertybasedtestingexperiments/databinding/util/QuickTheoriesTestSubscriberTest.kt
+++ app/src/test/kotlin/com/trov/propertybasedtestingexperiments/databinding/util/QuickTheoriesTestSubscriberTest.kt
@@ -7,15 +7,14 @@ import org.quicktheories.quicktheories.generators.SourceDSL.booleans
 import org.quicktheories.quicktheories.generators.SourceDSL.lists
 
 
-class QuickTheoriesTestSubscriber {
+class QuickTheoriesTestSubscriberTest {
   @Test fun failingTestEarlierThanksToPropertyBasedTesting() {
     qt().forAll(lists().allListsOf(booleans().all()).ofSizeBetween(0, 100)).checkAssert({
-      val values: Array<Boolean> = it.toTypedArray()
-
       val observable = ObservableField<Boolean>()
       val subscriber = TestSubscriber.create(observable)
 
-      values.forEach({ it -> observable.set(it) })
+      val values: Array<Boolean> = it.toTypedArray()
+      values.forEach({ value -> observable.set(value) })
 
       subscriber.assertValues(*values)
     })
